a61803e28214befc26fa8285d4aae509d33b7020
duyanh1909 <18520455@gm.uit.edu.vn>
2021-11-10 16:26:00 +0700

    update second

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import express from 'express';
 const app = express();
 const port = "3000"
 
-app.get("/", (req, res) => {
+app.get("/api/hello", (req, res) => {
     res.send({ message: "Hello World!" });
+}); 
+
+app.post("/api/hello", (req, res) => {
+    res.send({ message: "Post!"});
 });
 
 app.listen(port, () => {
